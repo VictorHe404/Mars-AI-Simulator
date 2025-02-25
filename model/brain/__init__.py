48409ff8a2532a7f384fef6201216b5aaf697330
aoaoaohuan <huan2584916242@gmail.com>
2025-02-24 21:32:09 -0500

    updated BrainAstar in parent(import), and in __all__.

--- model/brain/__init__.py
+++ model/brain/__init__.py
@@ -2,5 +2,5 @@ from model.fake import Log, Task, Environment, Avatar, DetectionMask
 from .brain import Brain
 from .brain_test import BrainTest
 from .brain_greedy import BrainGreedy
-
-__all__ = ["Brain", "BrainTest", "BrainGreedy",  "Log", "Task", "Environment", "Avatar", "DetectionMask"]
+from .brain_Astar import BrainAStar
+__all__ = ["Brain", "BrainTest", "BrainGreedy", "BrainAStar", "Log", "Task", "Environment", "Avatar", "DetectionMask"]
