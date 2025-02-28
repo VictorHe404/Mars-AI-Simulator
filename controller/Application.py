00131fc86d86ac59da9e3f42a010769aa73fd990
Yihong-anthony <li.yihong2653@icloud.com>
2025-02-28 18:25:39 -0500

    integrated Welcome Screen

--- controller/Application.py
+++ controller/Application.py
@@ -1,22 +1,43 @@
-from EventManager import *
-from KeyboardController import KeyboardController
+from  .EventManager import *
+from .KeyboardController import KeyboardController
+from .Visualizer import Visualizer
 
 class Application:
     """
     Application class: the main class of the application
     """
     def __init__(self) -> None:
+        self.running = False
         self.event_manager = EventManager()
-
         self.keyboard_controller = KeyboardController(self.event_manager)
-        # self.visualizer = Visualizer(self.event_manager)
+        self.visualizer = Visualizer(self.event_manager)
+
+    def notify(self, event: Event) -> None:
+        """
+        Notify the listener with the given event
+        """
+        if isinstance(event, Quit):
+            self.running = False
+            self.event_manager.unregister(self)
+
+        elif isinstance(event, InitialEvent):
+            print("Application is initializing")
+            self.running = True
+        elif isinstance(event, TicketEvent):
+            pass
+
 
     def run(self) -> None:
         """
         Run the application
         """
-        self.keyboard_controller.run()
-        # self.visualizer.run()
+        self.event_manager.post_event(InitialEvent("Application is running"))
+        self.running = True
+
+        while self.running:
+            self.event_manager.process_event()
+            event = TicketEvent("Application is running")
+
 
     def __str__(self):
         return "Application"
