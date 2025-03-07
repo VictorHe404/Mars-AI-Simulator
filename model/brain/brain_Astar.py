db79c509d6fb23889c61983cedb2c37392d3757a
aoaoaohuan <huan2584916242@gmail.com>
2025-03-06 22:09:07 -0500

    fully finished Basic Astar algorithm

--- model/brain/brain_Astar.py
+++ model/brain/brain_Astar.py
@@ -1,90 +1,174 @@
-from model.fake import Log, Task, Environment, Avatar, DetectionMask
+#from model.fake import Log, Task, Environment, Avatar, DetectionMask
+import math
+
+from model.simulator import Simulator, Task, Environment, Log
+from model.avatar import Avatar, DetectionMask
 from . import Brain
 import time as Time
-from collections import deque
 import heapq  # priority queue
+
 class BrainAStar(Brain):
     def run(self):
-        # defalt case : no current task
         if not self.current_task:
             return []
 
-        # Initialize the start and end points
         start = (self.current_task.start_row, self.current_task.start_col)
         end = (self.current_task.des_row, self.current_task.des_col)
 
-        # Initialize the detection map
         self.detect_map = [[0 for _ in range(len(self.original_map[0]))] for _ in range(len(self.original_map))]
         detection_mask = DetectionMask()
 
-        # Initialize energy related variables
         energy = self.current_avatar.battery_capacity
         max_energy = energy
         energy_recharge = self.current_avatar.energy_recharge_rate
 
-        # The priority queue (heap) required by the A* algorithm stores (f_score, g_score, current_position, path)
+        def is_adjacent(x1, y1, x2, y2):
+            return (abs(x1 - x2) + abs(y1 - y2)) == 1
+
         open_set = []
         heapq.heappush(open_set, (0, 0, start, []))
 
-        # Record the nodes that have been visited
         visited = set()
+        backtrack = set()
+        current_x = 0
+        current_y = 0
 
-        # Start A* Search
         while open_set:
-            # Take the node with the smallest f_score from the priority queue
-            f_score, g_score, (x, y), path = heapq.heappop(open_set)
+            chosen_node = None
+            while open_set:
 
-            # If you reach the end point, the mission is successful.
+                f_score, g_score, (x, y), path = heapq.heappop(open_set)
+
+                #if no parent
+                if not path:
+                    chosen_node = (f_score, g_score, x, y, path)
+                    break
+
+                #if has parent
+                px, py = current_x, current_y
+
+                #filter
+                if is_adjacent(px, py, x, y):
+                    chosen_node = (f_score, g_score, x, y, path)
+                    break
+                else:
+                    continue
+
+            #break condition
+            if not chosen_node:
+                print("The destination is unreachable (no valid adjacent nodes), task failed")
+                return self.task_trail
+
+            #apply chosen node
+            f_score, g_score, x, y, path = chosen_node
+
+            #if find the end
             if (x, y) == end:
                 print("The destination is reachable, task succeeded")
-                # Record the final log
-                log_entry = Log(index_x=x, index_y=y, detect_map=[row[:] for row in self.detect_map], time=self.time, energy=energy)
+                log_entry = Log(index_x=x, index_y=y, detect_map=[row[:] for row in self.detect_map],
+                                time=self.time, energy=energy)
                 self.task_trail.append(log_entry)
                 return self.task_trail
 
-            # If the current node has been visited, skip it
             if (x, y) in visited:
+                #filter for duplicate
                 continue
-
-            # Mark the current node as visited
             visited.add((x, y))
 
-            # Applying a detection mask
+
             self.detect_map = detection_mask.apply_mask(self.detect_map, self.original_map, x, y)
 
-            # Logging
-            log_entry = Log(index_x=x, index_y=y, detect_map=[row[:] for row in self.detect_map], time=self.time, energy=energy)
+            #update log
+            log_entry = Log(index_x=x, index_y=y, detect_map=[row[:] for row in self.detect_map],
+                            time=self.time, energy=energy)
             self.task_trail.append(log_entry)
 
-            # Traverse four directions (up, down, left, and right)
+            #is dead end?
+            is_dead_end = True
+            for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                nx, ny = x + dx, y + dy
+                if (0 <= nx < len(self.detect_map) and
+                    0 <= ny < len(self.detect_map[0]) and
+                     (nx, ny) not in visited and
+                    (nx, ny) not in backtrack and
+                    self.movable(x, y, nx, ny)):
+                    is_dead_end = False
+                    break
+
+            #trace back if dead end
+            if is_dead_end:
+                print(f"Dead end at ({x}, {y}), backtracking...")
+                backtrack.add((x, y))
+
+                while path:
+                    last_x, last_y = path[-1]
+                    is_last_dead_end = True
+                    for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                        lx, ly = last_x + dx, last_y + dy
+                        if (0 <= lx < len(self.detect_map) and
+                            0 <= ly < len(self.detect_map[0]) and
+                            (lx, ly) not in visited and
+                            (lx, ly) not in backtrack and
+                            self.movable(last_x, last_y, lx, ly)):
+                            is_last_dead_end = False
+                            break
+                    if not is_last_dead_end:
+                        break
+                    backtrack.add((last_x, last_y))
+                    path.pop()
+                    #update coordinates
+                    x, y = last_x, last_y
+                    log_entry = Log(index_x=x, index_y=y, detect_map=[row[:] for row in self.detect_map],
+                                    time=self.time, energy=energy)
+                    self.task_trail.append(log_entry)
+
+                #record coordinates
+                if path:
+                    x, y = path[-1]
+                log_entry = Log(index_x=x, index_y=y, detect_map=[row[:] for row in self.detect_map],
+                                time=self.time, energy=energy)
+                self.task_trail.append(log_entry)
+
+                current_x = x
+                current_y = y
+
+                continue
+
+            current_x = x
+            current_y = y
+
+
+            #find path
             for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
                 nx, ny = x + dx, y + dy
 
-                # Check if the new location is within the map and has not been visited before
-                if 0 <= nx < len(self.detect_map) and 0 <= ny < len(self.detect_map[0]):
-                    if (nx, ny) not in visited and self.movable(x, y, nx, ny):
-                        # Calculate the cost from the starting point to the new location
-                        new_g_score = g_score + self.cost(x, y, nx, ny)
+                if (0 <= nx < len(self.detect_map) and
+                    0 <= ny < len(self.detect_map[0]) and
+                    (nx, ny) not in visited and
+                    (nx, ny) not in backtrack and
+                    self.movable(x, y, nx, ny)):
 
-                        # Calculate the heuristic function (Manhattan distance)
-                        h_score = abs(nx - end[0]) + abs(ny - end[1])
+                    new_g_score = g_score + self.cost(x, y, nx, ny)
+                    h_score = abs(nx - end[0]) + abs(ny - end[1])
+                    new_f_score = new_g_score + h_score
 
-                        new_f_score = new_g_score + h_score
+                    #push avalable to the que
+                    heapq.heappush(open_set,
+                        (new_f_score, new_g_score, (nx, ny), path + [(x, y)])
+                    )
 
-                        # Add the new node to the priority queue
-                        heapq.heappush(open_set, (new_f_score, new_g_score, (nx, ny), path + [(x, y)]))
+                    #some updates
+                    energy -= (self.cost(x, y, nx, ny) + 1)
+                    self.time += (self.cost(x, y, nx, ny) + 1)
 
-                        # Renewal Energy
-                        energy -= (self.cost(x, y, nx, ny) + 1)
-                        self.time += (self.cost(x, y, nx, ny) + 1)
+                    if energy <= 0:
+                        while energy < max_energy:
+                            energy += energy_recharge
+                            self.time += 1
+                        energy = max_energy
 
-                        if energy <= 0:
-                            while energy < max_energy:
-                                energy += energy_recharge
-                                self.time += 1
-                            energy = max_energy
 
-        # fail case: If the priority queue is empty and the destination is not found
+        #if cant find the end
         print("The destination is unreachable, task failed")
         return self.task_trail
 
