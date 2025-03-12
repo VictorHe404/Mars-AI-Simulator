816f0cf3a43d386884a2f9b9409d64a623caf614
aoaoaohuan <huan2584916242@gmail.com>
2025-03-11 20:47:39 -0400

    Astar algorithm is finish and can produce pngs

--- model/test/integrated_test.py
+++ model/test/integrated_test.py
@@ -2,21 +2,20 @@ from model.avatar import Avatar, Sensor, DetectionMask
 from model.simulator import Simulator, environment, Log, MapManager, Task
 
 
-off_db_avatar = Avatar(
-    name="Mars Explorer X",
-    weight=80,
-    material="Titanium Alloy",
-    description="A high-endurance avatar designed for Mars exploration.",
-    battery_capacity=200,
-    battery_consumption_rate=5,
-    driving_force=280,
-    speed=1,
-    energy_recharge_rate=20,
-    sensors=[],
-    database_available=False
-)
-
 if __name__ == "__main__":
+    off_db_avatar = Avatar(
+        name="Mars Explorer X",
+        weight=80,
+        material="Titanium Alloy",
+        description="A high-endurance avatar designed for Mars exploration.",
+        battery_capacity=200,
+        battery_consumption_rate=5,
+        driving_force=280,
+        speed=1,
+        energy_recharge_rate=20,
+        sensors=[],
+        database_available=False
+    )
 
     radar_sensor = Sensor(
         name="Radar-360",
@@ -31,7 +30,7 @@ if __name__ == "__main__":
     off_db_avatar.bind_sensor(radar_sensor)
     simulator = Simulator()
     simulator.set_map("100x100Louth_Crater_ice_mound_subPart_sharp")
-    simulator.set_task(2,2,50,50)
+    simulator.set_task(2,2,90,90)
     simulator.set_avatar_no_db(off_db_avatar)
     simulator.set_brain("greedy")
     simulator.run()
